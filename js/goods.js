7f33e48ff33dd0aac2b0937e73726fbdae1eafc4
nadegdaMu <bykovanadegda@gmail.com>
2018-09-27 13:26:28 +0700

    Реализовано переключение вкладок в форме

--- js/goods.js
+++ js/goods.js
@@ -272,20 +272,27 @@ var paymentCard = payment.querySelector('.payment__card-wrap');
 var paymentCash = payment.querySelector('.payment__cash-wrap');
 
 payment.addEventListener('click', function (evt) {
-  if (evt.target.id === 'payment__card' || evt.target.id === 'payment__cash') {
-    paymentCard.classList.toggle('visually-hidden');
-    paymentCash.classList.toggle('visually-hidden');
+  if (evt.target.id === 'payment__card') {
+    paymentCard.classList.remove('visually-hidden');
+    paymentCash.classList.add('visually-hidden');
+  } else if (evt.target.id === 'payment__cash') {
+    paymentCard.classList.add('visually-hidden');
+    paymentCash.classList.remove('visually-hidden');
   }
+
 });
 
-// Переключение вкладок в блоке доставке
+// Переключение вкладок в блоке доставки
 var deliver = document.querySelector('.deliver');
 var deliverStore = deliver.querySelector('.deliver__store');
 var deliverCourier = deliver.querySelector('.deliver__courier');
 
 deliver.addEventListener('click', function (evt) {
-  if (evt.target.id === 'deliver__store' || evt.target.id === 'deliver__courier') {
-    deliverStore.classList.toggle('visually-hidden');
-    deliverCourier.classList.toggle('visually-hidden');
+  if (evt.target.id === 'deliver__store') {
+    deliverStore.classList.remove('visually-hidden');
+    deliverCourier.classList.add('visually-hidden');
+  } else if (evt.target.id === 'deliver__courier') {
+    deliverStore.classList.add('visually-hidden');
+    deliverCourier.classList.remove('visually-hidden');
   }
 });
