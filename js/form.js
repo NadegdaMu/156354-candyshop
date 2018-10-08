198a1dfe7243e843fe5104bb0f0e7cef20d9ad03
nadegdaMu <bykovanadegda@gmail.com>
2018-10-08 14:02:01 +0700

    Добавлены Disabled для полей

--- js/form.js
+++ js/form.js
@@ -1,9 +1,30 @@
 'use strict';
 
-form.addEventListener('submit', function (evt) {
-  evt.preventDefault();
-  if (form.checkValidity()) {
-    window.backend.uploadData(new FormData(form), onLoad, onError);
-  }
-  return false;
-});
+(function () {
+  var form = document.querySelector('.buy form');
+  var inputs = form.querySelectorAll('input');
+  var hiddenInputs = form.querySelectorAll('.visually-hidden input');
+  var fieldSets = form.querySelectorAll('fieldset');
+  var disabledState = window.shoppingСart ? window.shoppingСart.length === 0 : true;
+
+  var inputDisabled = function () {
+    for (var i = 0; i < inputs.length; i++) {
+      inputs[i].disabled = disabledState;
+    }
+    for (var j = 0; j < fieldSets.length; j++) {
+      fieldSets[j].disabled = disabledState;
+    }
+    for (var k = 0; k < hiddenInputs.length; k++) {
+      hiddenInputs[k].disabled = true;
+    }
+  };
+
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    if (form.checkValidity()) {
+      window.backend.uploadData(new FormData(form), onLoad, onError);
+    }
+    return false;
+  });
+})();
+
