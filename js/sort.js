98f6f36737d667d304ed88c8ad21518e4381eae4
nadegdaMu <bykovanadegda@gmail.com>
2018-10-10 17:40:47 +0700

    Доделана функция sortGoods

--- js/sort.js
+++ js/sort.js
@@ -3,6 +3,20 @@
 (function () {
   var prodactSortList = document.querySelector('.catalog__filter');
 
+  var kind = {
+    icecream: prodactSortList.querySelector('#filter-icecream ~ .input-btn__item-count'),
+    soda: prodactSortList.querySelector('#filter-soda ~ .input-btn__item-count'),
+    gum: prodactSortList.querySelector('#filter-gum ~ .input-btn__item-count'),
+    marmalade: prodactSortList.querySelector('#filter-marmalade ~ .input-btn__item-count'),
+    marshmallows: prodactSortList.querySelector('#filter-marshmallows ~ .input-btn__item-count')
+  };
+
+  var nutrition = {
+    sugarFree: document.querySelector('#filter-sugar-free ~ .input-btn__item-count'),
+    vegetarian: document.querySelector('#filter-vegetarian ~ .input-btn__item-count'),
+    glutenFree: document.querySelector('#filter-gluten-free ~ .input-btn__item-count')
+  };
+
   var nameMap = {
     1: 'Зефир',
     2: 'Жевательная резинка',
@@ -15,35 +29,35 @@
   var getSortNameGoods = function (array, arrayName) {
     var arrayNameGoods = array.filter(function (el) {
       if (el.kind === arrayName) {
-        array.push(el);
+        return true;
       }
     })
     return arrayNameGoods;
   };
 
   // Функция сортирующая по отсутствию сахара в продукте
-  var getSugarProdact = function () {
+  var getSugarProdact = function (array) {
     var arraySugar = array.filter(function (el) {
       if (el.sugar === false) {
-        array.push(el);
+        return true;
       }
     })
     return arraySugar;
   }
   // Функция сортирующая по отсутствию глютена
-   var getGlutenProdact = function () {
+   var getGlutenProdact = function (array) {
     var arrayGluten = array.filter(function (el) {
       if (el.gluten === false) {
-        array.push(el);
+        return true;
       }
     })
     return arrayGluten;
   }
   // Функция сортирующая по отсутствию животных жиров
-   var getVegetarianProdact = function () {
+   var getVegetarianProdact = function (array) {
     var arrayVegetarian = array.filter(function (el) {
       if (el.vegetarian === true) {
-        array.push(el);
+        return true;
       }
     })
     return arrayVegetarian;
@@ -51,15 +65,18 @@
 
   // Функция подсчитывающая колличество ел-ов
   var countElementArray = function (array) {
-    var numberElement = array.lenght;
+    var numberElement = array.length;
     return numberElement;
   }
 
   window.sortGoods = function () {
-    prodactSortList.querySelector('#filter-icecream ~ input-btn__item-count').text = countElementArray(getSortNameGoods(window.products, nameMap[3]));
-    prodactSortList.querySelector('#filter-soda ~ input-btn__item-count').text = countElementArray(getSortNameGoods(window.products, nameMap[4]));
-    prodactSortList.querySelector('#filter-gum ~ input-btn__item-count').text = countElementArray(getSortNameGoods(window.products, nameMap[2]));
-    prodactSortList.querySelector('#filter-marmalade ~ input-btn__item-count').text = countElementArray(getSortNameGoods(window.products, nameMap[5]));
-    prodactSortList.querySelector('#filter-marshmallows ~ input-btn__item-count').text = countElementArray(getSortNameGoods(window.products, nameMap[1]));
+    kind.icecream.textContent ='(' + countElementArray(getSortNameGoods(window.products, nameMap[3])) + ')';
+    kind.soda.textContent ='(' + countElementArray(getSortNameGoods(window.products, nameMap[4])) + ')';
+    kind.gum.textContent ='(' + countElementArray(getSortNameGoods(window.products, nameMap[2])) + ')';
+    kind.marmalade.textContent ='(' + countElementArray(getSortNameGoods(window.products, nameMap[5])) + ')';
+    kind.marshmallows.textContent ='(' + countElementArray(getSortNameGoods(window.products, nameMap[1])) + ')';
+    nutrition.sugarFree.textContent ='(' + countElementArray(getSugarProdact(window.products)) + ')';
+    nutrition.vegetarian.textContent ='(' + countElementArray(getVegetarianProdact(window.products)) + ')';
+    nutrition.glutenFree.textContent ='(' + countElementArray(getGlutenProdact(window.products)) + ')';
   }
 })();
